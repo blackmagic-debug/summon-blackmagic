35ba99d07a1370d7f09b7a1a620df0ef243aca2f
dragonmux <git@dragonmux.network>
2025-04-06 10:31:57 +0100

    summon: Implemented configuration of Flask from a config file

--- summon/__init__.py
+++ summon/__init__.py
@@ -1,7 +1,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from flask import Flask, render_template
 
+# Initialise Flask for summon
 app = Flask(__name__)
+# Configure Flask from the config.py in this directory
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
