6e400585692e2d95377825c0eb6584d8365c2256
dragonmux <git@dragonmux.network>
2025-04-06 13:10:07 +0100

    summon/models: Built out a model for describing a firmware download that is available

--- summon/models.py
+++ summon/models.py
@@ -1,9 +1,41 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, types
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from pathlib import Path
+from typing import NewType
 
-db = SQLAlchemy()
+from .types import Probe
 
+# Define types for mapping things in and out of the database cleanly
+i32 = NewType("i32", int)
+i64 = NewType("i64", int)
+
+# Define the type mappings from Python to the DBMS
+class Model(DeclarativeBase):
+	type_annotation_map = {
+		Path: types.String,
+		i32: types.Integer,
+		i64: types.BigInteger,
+		Probe: types.Integer,
+	}
+
+# Build the Flask SQLAlchemy object, making use of the model mappings
+db = SQLAlchemy(model_class = Model)
+
+# Releases of BMD that have been made
 class Release(db.Model):
-	id: Mapped[int] = mapped_column(primary_key = True)
+	id: Mapped[i32] = mapped_column(primary_key = True)
 	version: Mapped[str]
+
+# Downloads for firmware available by probe platform
+class FirmwareDownload(db.Model):
+	id: Mapped[i64] = mapped_column(primary_key = True)
+	releaseID: Mapped[i32] = mapped_column(ForeignKey('Release.id'))
+	friendlyName: Mapped[str]
+	fileName: Mapped[Path]
+	uri: Mapped[str]
+	probe: Mapped[Probe]
+	# If there are multiple firmware downloads for one probe in one release, this
+	# provides a name to which variant this download is for
+	variantName: Mapped[str]
