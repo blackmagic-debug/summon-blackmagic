62ad340eab5454740a0bd1c6e1525a7b362a0230
dragonmux <git@dragonmux.network>
2025-04-07 15:45:33 +0100

    summon/githubTypes: Created a set of typing types for describing the JSON we get out of the GitHub APIs

---
+++ summon/githubTypes.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from typing import TypedDict
+
+__all__ = (
+	'GitHubUser',
+	'GitHubAsset',
+	'GitHubRelease',
+)
+
+class GitHubUser(TypedDict):
+	id: int
+	node_id: str
+	type: str
+	url: str
+	html_url: str
+	login: str
+	avatar_url: str
+	gravatar_id: str
+	followers_url: str
+	following_url: str
+	gists_url: str
+	starred_url: str
+	subscriptions_url: str
+	organizations_url: str
+	repos_url: str
+	events_url: str
+	received_events_url: str
+	site_admin: bool
+
+class GitHubAsset(TypedDict):
+	id: int
+	node_id: str
+	type: str
+	url: str
+	name: str
+	browser_download_url: str
+	label: str
+	state: str
+	content_type: str
+	size: int
+	download_count: int
+	created_at: str
+	updated_at: str
+	uploader: GitHubUser
+
+class GitHubRelease(TypedDict):
+	id: int
+	node_id: str
+	url: str
+	name: str
+	html_url: str
+	assets_url: str
+	upload_url: str
+	tarball_url: str
+	zipball_url: str
+	tag_name: str
+	target_commitish: str
+	body: str
+	draft: bool
+	prerelease: bool
+	created_at: str
+	published_at: str
+	author: GitHubUser
+	assets: list[GitHubAsset]
