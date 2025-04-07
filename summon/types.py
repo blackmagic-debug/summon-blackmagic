d7aca3daf85ef69c83e2d4d011be5c5908d1f14c
dragonmux <git@dragonmux.network>
2025-04-07 18:16:59 +0100

    summon/types: Implemented a conversion from a string to the Probe enum

--- summon/types.py
+++ summon/types.py
@@ -23,3 +23,39 @@ class Probe(IntEnum):
 	stlink = 12
 	stlinkv3 = 13
 	swlink = 14
+
+	@staticmethod
+	def fromString(name: str) -> 'Probe':
+		match name:
+			case '96b_carbon':
+				return Probe._96bCarbon
+			case 'blackpill-f401cc':
+				return Probe.blackpillF401CC
+			case 'blackpill-f401ce':
+				return Probe.blackpillF401CE
+			case 'blackpill-f411ce':
+				return Probe.blackpillF411CE
+			case 'bluepill':
+				return Probe.bluepill
+			case 'ctxlink':
+				return Probe.ctxLink
+			case 'f072':
+				return Probe.f072
+			case 'f3':
+				return Probe.f3
+			case 'f4discovery':
+				return Probe.f4Discovery
+			case 'hydrabus':
+				return Probe.hydraBus
+			case 'launchpad-icdi':
+				return Probe.launchpadICDI
+			case 'native':
+				return Probe.native
+			case 'stlink' | 'stlinkv2':
+				return Probe.stlink
+			case 'stlinkv3':
+				return Probe.stlinkv3
+			case 'swlink':
+				return Probe.swlink
+			case _:
+				raise ValueError(f'Invalid probe name {name}')
