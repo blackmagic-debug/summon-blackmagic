df66d5798864f0734cd6bf6c9bfac629bac63339
dragonmux <git@dragonmux.network>
2025-04-10 11:41:23 +0100

    summon/githubTypes: Created a type for describing an organisation

--- summon/githubTypes.py
+++ summon/githubTypes.py
@@ -27,6 +27,20 @@ class GitHubUser(TypedDict):
 	received_events_url: str
 	site_admin: bool
 
+class GitHubOrganisation(TypedDict):
+	id: int
+	node_id: str
+	url: str
+	login: str
+	repos_url: str
+	events_url: str
+	hooks_url: str
+	issues_url: str
+	members_url: str
+	public_members_url: str
+	avatar_url: str
+	description: str
+
 class GitHubAsset(TypedDict):
 	id: int
 	node_id: str
