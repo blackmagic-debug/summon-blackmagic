62285068d3f20c00a132523df590b597273c8815
dragonmux <git@dragonmux.network>
2025-04-10 11:49:37 +0100

    summon/githubTypes: Created a type for describing a repository

--- summon/githubTypes.py
+++ summon/githubTypes.py
@@ -41,6 +41,95 @@ class GitHubOrganisation(TypedDict):
 	avatar_url: str
 	description: str
 
+class GitHubLicense(TypedDict):
+	node_id: str
+	url: str
+	key: str
+	name: str
+	spdx_id: str
+
+class GitHubRepository(TypedDict):
+	id: int
+	node_id: str
+	url: str
+	html_url: str
+	name: str
+	full_name: str
+	private: bool
+	owner: GitHubUser
+	description: str
+	fork: bool
+	forks_url: str
+	keys_url: str
+	collaborators_url: str
+	teams_url: str
+	hooks_url: str
+	issue_events_url: str
+	events_url: str
+	assignees_url: str
+	branches_url: str
+	tags_url: str
+	blobs_url: str
+	git_tags_url: str
+	git_refs_url: str
+	tress_url: str
+	statuses_url: str
+	languages_url: str
+	stargazers_url: str
+	contributors_url: str
+	subscribers_url: str
+	subscription_url: str
+	commits_url: str
+	git_commits_url: str
+	comments_url: str
+	issue_comment_url: str
+	contents_url: str
+	compare_url: str
+	merges_url: str
+	archive_url: str
+	downloads_url: str
+	issues_url: str
+	pulls_url: str
+	milestones_url: str
+	notifications_url: str
+	labels_url: str
+	releases_url: str
+	deployments_url: str
+	created_at: str
+	updated_at: str
+	pushed_at: str
+	git_url: str
+	ssh_url: str
+	clone_url: str
+	svn_url: str
+	homepage: str
+	size: int
+	stargazers_count: int
+	watchers_count: int
+	language: str
+	has_issues: bool
+	has_projects: bool
+	has_downloads: bool
+	has_wiki: bool
+	has_pages: bool
+	has_discussions: bool
+	forks_count: int
+	mirror_url: str | None
+	archived: bool
+	disabled: bool
+	open_issues_count: int
+	license: GitHubLicense
+	allow_forking: bool
+	is_template: bool
+	web_commit_signoff_required: bool
+	topics: list[dict]
+	visibility: str
+	forks: int
+	open_issues: int
+	watchers: int
+	default_branch: str
+	custom_properties: dict
+
 class GitHubAsset(TypedDict):
 	id: int
 	node_id: str
